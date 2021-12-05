ccf7a0eb1997152a7968ebd0adcd092b20638d5c
jsruizacero <jsruizacero@gmail.com>
2021-12-05 19:32:26 -0300

    Organización de archivos de tipos y resolvers por carpetas pra cada modelo. Adición de tipos y resolvers para el modelo de Avance

---
+++ models/avance/resolvers.ts
@@ -0,0 +1,23 @@
+import { ModeloAvance } from './avance';
+
+const resolversAvance = {
+  Query: {
+    Avances: async (parent, args) => {
+        ModeloAvance.find().populate('proyecto').populate('creadoPor');
+        return avances;
+    },
+  },
+  Mutation: {
+    crearAvance: async (parents, args) => {
+      const avanceCreado = ModeloAvance.create({
+        fecha: args.fecha,
+        descripcion: args.descripcion,
+        proyecto: args.proyecto,
+        creadoPor: args.creadoPor,
+      });
+      return avanceCreado;
+    },
+  },
+};
+
+export { resolversAvance };
